fe238efd2e605450505950e853064b6b472094fc
cluoma <themjolnir999@gmail.com>
2019-04-22 18:21:02 +0200

    Add link to steam group

--- templates/about_box.m
+++ templates/about_box.m
@@ -5,5 +5,5 @@
     <p>The options for Linux gamers are always getting better and we hope to show that.</p>
     <a href="https://twitter.com/LinuxGameDotNet"><img src="/images/twitter_logo.svg" alt="Twitter logo" width=35px height=35px></a>
     <a href="https://discord.gg/T2uV48Y"><img src="/images/discord_logo.svg" alt="Discord logo" width=35px height=35px></a>
-    <a href=""><img src="/images/steam_logo.svg" alt="Discord logo" width=35px height=35px></a>
+    <a href="https://steamcommunity.com/groups/linuxgamenet"><img src="/images/steam_logo.svg" alt="Discord logo" width=35px height=35px></a>
 </div>
