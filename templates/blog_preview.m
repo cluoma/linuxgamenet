518061ac46c4221a84975dda0aff5ca52f30b676
cluoma <themjolnir999@gmail.com>
2019-04-22 18:35:22 +0200

    Add alt text to blog preview thumbnail

--- templates/blog_preview.m
+++ templates/blog_preview.m
@@ -31,14 +31,14 @@
         {{#posts}}
         <div class="blog-post-preview-card">
             {{#rewrite}}
-            <a href="/post/{{p_id}}"><img class="blog-post-preview-img" src="/images/{{thumbnail}}"></a>
+            <a href="/post/{{p_id}}"><img class="blog-post-preview-img" src="/images/{{thumbnail}}" alt="Thumbnail for post: {{title}}"></a>
             <a href="/post/{{p_id}}"><h2 class="blog-post-preview-title">{{title}}</h2></a>
             <p class="blog-post-meta">{{time}}</p>
             <p>{{byline}}</p>
             <p style="clear: both; margin: 0px;"><b>Tags:</b> {{#tags}}<a class="blog-post-tag" href="/tag/{{.}}">{{.}}</a> {{/tags}}</p>
             {{/rewrite}}
             {{^rewrite}}
-            <a href="{{script_name}}?page={{page_name}}&id={{p_id}}"><img class="blog-post-preview-img" src="/images/{{thumbnail}}"></a>
+            <a href="{{script_name}}?page={{page_name}}&id={{p_id}}"><img class="blog-post-preview-img" src="/images/{{thumbnail}}" alt="Thumbnail for post: {{title}}"></a>
             <a href="{{script_name}}?page={{page_name}}&id={{p_id}}"><h2 class="blog-post-preview-title">{{title}}</h2></a>
             <p class="blog-post-meta">{{time}}</p>
             <p>{{byline}}</p>
