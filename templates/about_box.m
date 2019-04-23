f80d1766915f3510e8649ca586bc249dd0738347
n <n@n.n>
2019-04-23 21:04:19 +0200

    Add rss logo to about box

--- templates/about_box.m
+++ templates/about_box.m
@@ -6,4 +6,5 @@
     <a href="https://twitter.com/LinuxGameDotNet"><img src="/images/twitter_logo.svg" alt="Twitter logo" width=35px height=35px></a>
     <a href="https://discord.gg/T2uV48Y"><img src="/images/discord_logo.svg" alt="Discord logo" width=35px height=35px></a>
     <a href="https://steamcommunity.com/groups/linuxgamenet"><img src="/images/steam_logo.svg" alt="Steam logo" width=35px height=35px></a>
+    <a href="https://www.linuxgame.net/blog/rss"><img src="/images/rss_logo.svg" alt="RSS logo" width=35px height=35px></a>
 </div>
