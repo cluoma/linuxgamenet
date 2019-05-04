a2addad3904ae78f363069b7c05705319b9b9b31
cluoma <themjolnir999@gmail.com>
2019-05-04 13:08:49 +0200

    update setup and update scripts

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
+sqlite3 bittyblog.db "CREATE TABLE IF NOT EXISTS \`last_update\` (\`time\` INTEGER NOT NULL);"
+
+sqlite3 bittyblog.db "DELETE FROM last_update;"
+sqlite3 bittyblog.db "INSERT INTO last_update (time) VALUES(CAST(strftime('%s', 'now') AS INT));"
+
 echo .. Compiling bittyblog
 make all
 echo .. Copying executables
