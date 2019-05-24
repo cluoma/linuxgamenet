cd6f0c901a672842663ac9b9a1008dac8294a1f1
cluoma <themjolnir999@gmail.com>
2019-05-24 19:43:02 +0200

    Add template for twitch banner

---
+++ templates/twitch_banner.m
@@ -0,0 +1,1 @@
+<a href="https://www.twitch.tv/lgngarpike"><div style="background-color: #4b367c; color: white; margin-left: 15px; padding: 5px; text-align: center; font-size: 14pt;"><b>Live now on Twitch!</b></div></a><br>
