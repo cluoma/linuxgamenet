3b11120d50e551deac86f49411c75d77cb26dc2f
cluoma <themjolnir999@gmail.com>
2019-06-03 23:33:45 +0200

    Fix css for twitch banner

--- templates/twitch_banner.m
+++ templates/twitch_banner.m
@@ -1,6 +1,5 @@
 <a href="https://www.twitch.tv/lgngarpike">
-<div style="background-color: #4b367c; color: white; margin-left: 15px; padding: 5px; text-align: center; font-size: 14pt;">
+<div style="background-color: #4b367c; color: white; padding: 5px; text-align: center; font-size: 14pt; max-width: 100%; margin: 15px; margin-top: 0px; margin-bottom: 18px;">
 <b>Live now on Twitch!</b>
 </div>
 </a>
-<br>
