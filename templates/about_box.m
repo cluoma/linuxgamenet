9e3bf07a42b08e5a2743cb5dcac07ed79f6d5835
n <n@n.n>
2019-06-03 23:40:19 +0200

    update about box text

--- templates/about_box.m
+++ templates/about_box.m
@@ -1,7 +1,7 @@
 <!-- About box sidebar module-->
 <div class="sidebar-module sidebar-module-inset"><h4>About</h4>
-    <p>Linux Game Network provides the most recent news in the world of Linux gaming.</p>
-    <p>Covering new game releases, hardware, and more in-depth reviews of big titles.</p>
+    <p>LinuxGameNetwork is a place to share recent news in the world of Linux gaming.</p>
+    <p>Covering new game releases, hardware, and more in-depth reviews.</p>
     <p>Options for Linux gamers have never been better than they are today.</p>
     <a href="https://twitter.com/LinuxGameDotNet"><img src="/images/twitter_logo.svg" alt="Twitter logo" width=35px height=35px></a>
     <a href="https://discord.gg/T2uV48Y"><img src="/images/discord_logo.svg" alt="Discord logo" width=35px height=35px></a>
