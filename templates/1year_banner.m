19e5c3436d59f3d930e96f9de68a3e4d377502d0
Colin Luoma <themjolnir999@gmail.com>
2020-04-12 01:55:54 +0200

    Add template for 1year banner

---
+++ templates/1year_banner.m
@@ -0,0 +1,5 @@
+<a href="https://www.twitch.tv/lgngarpike">
+<div style="color: white; padding: 5px; max-width: 100%; margin: 15px; margin-top: -11px; margin-bottom: 7px;">
+<img src="images/1year_banner.png" style="max-width: 100%;">
+</div>
+</a>
