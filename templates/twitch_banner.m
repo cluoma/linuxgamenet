8c94747f5245820be2fae293bcc73bfb9789499d
n <themjolnir999@gmail.com>
2020-10-30 20:43:39 +0100

    Make twitch_banner.m template empty

--- templates/twitch_banner.m
+++
@@ -1,6 +0,0 @@
-<!--offline--><!--
-<a href="https://www.twitch.tv/lgngarpike">
-<div style="background-color: #4b367c; color: white; padding: 4px; padding-top: 6px; text-align: center; font-size: 14pt; max-width: 100%; margin: 15px; margin-top: 0px; margin-bottom: 18px;">
-<b>Live now on Twitch!</b>{game}
-</div>
-</a>-->
